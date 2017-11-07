417ff8614701cc6240903056457028b7cd9dc9bd
Mahesh Vangala <vangalamaheshh@gmail.com>
2017-11-07 14:46:19 -0500

    added test username

--- oauthenticator/generic.py
+++ oauthenticator/generic.py
@@ -119,7 +119,7 @@ class GenericOAuthenticator(OAuthenticator):
                           )
         resp = yield http_client.fetch(req)
         resp_json = json.loads(resp.body.decode('utf8', 'replace'))
-
+        resp_json["username"] = "mvangala"
         if not resp_json.get(self.username_key):
             self.log.error("OAuth user contains no key %s: %s", self.username_key, resp_json)
             return
