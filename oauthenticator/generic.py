107c7e06e1364cbeb1efdaea5eaa27ab3b0c695d
Mahesh Vangala <vangalamaheshh@gmail.com>
2017-11-08 15:29:24 -0500

    debug

--- oauthenticator/generic.py
+++ oauthenticator/generic.py
@@ -119,11 +119,11 @@ class GenericOAuthenticator(OAuthenticator):
                           )
         resp = yield http_client.fetch(req)
         resp_json = json.loads(resp.body.decode('utf8', 'replace'))
-        resp_json["username"] = "mvangala"
-        #if not resp_json.get(self.username_key):
-        #    self.log.error("OAuth user contains no key %s: %s", self.username_key, resp_json)
-        #    return
-
+        if not resp_json.get(self.username_key):
+            self.log.error("OAuth user contains no key %s: %s", self.username_key, resp_json)
+            return
+        with open("/srv/jupyterhub/test.txt", "w") as ofh:
+          ofh.write(resp_json)
         return {
             'name': resp_json.get(self.username_key),
             'auth_state': {
